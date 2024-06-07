f82833dbdc35f53c199d779d5bcb37307bbb97a3
Evgeny Samoshin <samevgeny@gmail.com>
2024-06-07 15:47:36 +0400

    add main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const productName = "Cisco Catalist 3750r";
+const productQuantity = 1;
+const productCategory = "Network equipment";
+const productPrice = 4000;
+
+console.log("Product Name: " + productName);
+
+console.log("Total coast: " + productQuantity * productPrice + "$");
