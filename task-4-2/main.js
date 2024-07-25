366004d7f72b5d4a7528eb5290fa85e3db0ff804
Evgeny Samoshin <samevgeny@gmail.com>
2024-07-25 14:25:42 +0400

    add task-4-2

--- task-4-2/main.js
+++ task-4-2/main.js
@@ -1,7 +1,18 @@
 const productName = prompt('Введите наименование товара: ');
+
 const productQuantity = +prompt('Введите количество товара: ');
+
+if (!Number.isInteger(productQuantity)) {
+  console.log('Вы ввели некорректные данные');
+}
+
 const productCategory = prompt('Введите категорию товара: ');
+
 const productPrice = +prompt('Введите цену товара: ');
+
+if (!Number.isInteger(productPrice)) {
+  console.log('Вы ввели некорректные данные');
+}
 
 console.log("Product Name: " + productName);
 
