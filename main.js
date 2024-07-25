b3b61df2944c65c3db83e9782125a4a6afd6b261
Evgeny Samoshin <samevgeny@gmail.com>
2024-07-25 22:56:54 +0400

    add task-5-2

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ const productPrice = 4000;
 console.log("Product Name: " + productName);
 
 console.log("Total coast: " + productQuantity * productPrice + "$");
+
+const modalTitle = document.querySelector('.modal__title');
+const closeBtn = document.querySelector('.modal__close-btn');
+const productId = document.querySelector('.product-id');
+const modalForm = document.querySelector('.form');
+const discountCheckbox = document.querySelector('#discount');
+const discountValue = document.querySelector('#discountValue');
+const totalCoast = document.querySelector('.total-cost__sum');
